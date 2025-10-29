818cc6d0123086a2ee2f38d578e37754d61896da
kasukefreedombarst0-tech <kasukefreedombarst0@gmail.com>
2025-10-29 10:58:31 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+let taizyuu;
+
+taizyuu = 68; // kg
+
+let BMI;
+
+let sinntoyu = 1.7; // m
+
+BMI = taizyuu / (sinntoyu * sinntoyu);
+
+console.log("BMI値は" + BMI + "です");
+
+taizyuu = 70; // kg
+
+BMI = taizyuu / (sinntoyu * sinntoyu);
+
+console.log("BMI値は" + BMI + "です");
