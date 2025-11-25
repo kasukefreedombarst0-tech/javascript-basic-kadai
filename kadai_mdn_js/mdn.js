f77d444efa1bb41797abfe1cb09295dcca4af26a
kasukefreedombarst0-tech <kasukefreedombarst0@gmail.com>
2025-11-25 12:01:39 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -3,4 +3,4 @@ const year = today.getFullYear();
 const month = today.getMonth() + 1; // getMonth() は 0 から始まるので、1 を足します
 const date = today.getDate();
 
-console.log(year + '年' + month + '月' + day + '日');
+console.log(year + '年' + month + '月' + date + '日');
